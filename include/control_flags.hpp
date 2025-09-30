e32fa4bfa4dc564929a62c1a82f1582d4abca74d
phrenezi <parizot.luan.2b@gmail.com>
2025-09-30 10:56:51 +0200

    idem

--- include/control_flags.hpp
+++ include/control_flags.hpp
@@ -5,7 +5,7 @@
 extern std::atomic<bool> flag_walk_forward;
 extern std::atomic<bool> flag_walk_backwards;
 extern std::atomic<bool> flag_walk_right;
-extern std::atomic<bool> flag_turn_left;
+extern std::atomic<bool> flag_walk_left;
 
 // Flag d'interruption utilisé *à l'intérieur* des longues routines (walk/run/turn).
 // Les fonctions d'action doivent vérifier stopRequested.load() périodiquement.
